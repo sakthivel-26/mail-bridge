5b81464716d98a6d48b728f374c84eef47f25bef
sakthi-28 <sakhtivelvenkat2619@gmail.com>
2026-04-02 15:07:57 +0530

    fixing google client id

--- mailbridge/static/config.js
+++ mailbridge/static/config.js
@@ -1,4 +1,5 @@
 window.MAILBRIDGE_CONFIG = {
   API_BASE_URL: "https://mail-bridge-wybz.onrender.com",  // ✅ FIXED
-  MCP_SERVER_URL: "https://mailbridge-mcp.onrender.com"
+  MCP_SERVER_URL: "https://mailbridge-mcp.onrender.com",
+  GOOGLE_CLIENT_ID: "931753516529-bnjr91coe5tiova8pm1f3lr6ba011se2.apps.googleusercontent.com"
 };
